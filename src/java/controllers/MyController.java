b44581d804c4e3490fab237b16e7d5a42934a999
it354f715 <it354f715@WEBDEV-24.ad.ilstu.edu>
2017-12-04 16:49:50 -0600

    made more progress

--- src/java/controllers/MyController.java
+++ src/java/controllers/MyController.java
@@ -5,14 +5,25 @@
  */
 package controllers;
 
+import api.CardAPI;
+import api.CardStore;
+import api.DeckAPI;
 import api.DeckStore;
+import api.SchoolClassAPI;
+import api.SchoolClassStore;
+import java.util.Iterator;
 import java.util.List;
-import javax.enterprise.inject.Model;
+import models.Card;
+
 import models.Deck;
+import models.SchoolClass;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 /**
  *
  * @author it354f715
@@ -20,18 +31,64 @@ import org.springframework.web.portlet.ModelAndView;
 @Controller
 public class MyController {
     
-    DeckStore deckStore = new DeckStore();
+    @Autowired
+    DeckStore deckStore;
+    @Autowired
+    CardStore cardStore;
+    @Autowired
+    SchoolClassStore schoolClassStore;
+    @Autowired
+    CardAPI cardAPI;
+    @Autowired
+    DeckAPI deckAPI;
+    @Autowired
+    SchoolClassAPI schoolClassAPI;
     List<Deck> deckList;
+    List<Card> cardList;
     
     @RequestMapping(value="/", method = RequestMethod.GET)
-    public ModelAndView viewIndex(){
+    public String viewIndex(Model model){
         
         deckList = deckStore.getAllDecks();
         
-        // return back to index.jsp
-        ModelAndView model = new ModelAndView("index");
-        model.addObject("deckList", deckList);
-        return model;
+        model.addAttribute("deckList", deckList);
+        
+        return "index";
+    }
+    
+    @RequestMapping(value="/viewDeckDetails/{id}", method=RequestMethod.GET)
+    public String viewDeckDetails(@PathVariable("id") String deckid, Model model) {
+        
+        cardList = cardStore.getCardsByDeckid(deckid);
+        Iterator<Deck> it = deckList.iterator();
+        Deck tempDeck;
+        String deckName = null;
+        while(it.hasNext()) {
+            tempDeck = it.next();
+            if (tempDeck.getId().equals(deckid)) {
+                deckName = tempDeck.getDeckname();
+            }
+        }        
+        model.addAttribute("deckName", deckName);
+        model.addAttribute("cardList", cardList);
+        
+        return "deckDetails";
+    }
+    
+    @RequestMapping(value="/addDeck", method=RequestMethod.GET)
+    public String viewAddDeckPage() {
+        return "addDeck";
+    }
+    
+    @RequestMapping(value="/addClass", method=RequestMethod.POST)
+    public String postClass(@RequestParam("classname") String classname, @RequestParam("classnumber") String classnumber) {
+        
+        SchoolClass tempClass = new SchoolClass(classnumber, classname);
+        if (schoolClassAPI.postClass(tempClass)) {
+            return "somewhere";
+        } else {
+            return "somewhereelse";
+        }
     }
     
 }
